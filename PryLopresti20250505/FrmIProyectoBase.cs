a0c6cbac229a87c8f17bfc61dae55b3039056bee
Alumno <Alumno@PC18>
2025-05-05 11:37:42 -0300

    Agregar archivos de proyecto.

---
+++ PryLopresti20250505/FrmIProyectoBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PryLopresti20250505
+{
+    public partial class FrmIProyectoBase : Form
+    {
+        public FrmIProyectoBase()
+        {
+            InitializeComponent();
+        }
+    }
+}
